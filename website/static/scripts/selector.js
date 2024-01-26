103f8fbfe439914105e1f18cedd047451cf56474
AJR07 <angjunray@live.com>
2024-01-26 20:08:49 +0800

    💥 NEW: Detection Box :D

--- website/static/scripts/selector.js
+++ website/static/scripts/selector.js
@@ -4,6 +4,8 @@ const itemSelector = document.getElementById("item-selector");
 slider.style.width = itemSelector.children[0].getBoundingClientRect().width + "px";
 slider.style.left = (itemSelector.children[0].getBoundingClientRect().x - 1) + "px";
 
+
+
 // slider animation
 for (let i = 0; i < itemSelector.children.length; i++) {
 	const element = itemSelector.children[i];
@@ -17,7 +19,19 @@ for (let i = 0; i < itemSelector.children.length; i++) {
 
 	// handle when clicked
 	element.addEventListener("click", () => {
-		let id = element.nodeValue.toLowerCase().replace(" ", "-");
-		document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+		let id = element.innerHTML.toLowerCase().replace(" ", "-");
+		const newElement = document.getElementById(id);
+		let oldID = document.getElementsByClassName("selected")[0].innerHTML.toLowerCase().replace(" ", "-");
+		const oldElement = document.getElementById(oldID);
+
+		// transition oldElement out by fading
+		oldElement.style.opacity = 0;
+
+		// transition newElement in by fading
+		newElement.style.opacity = 1;
+
+		// change selected classname
+		document.getElementsByClassName("selected")[0].classList.remove("selected");
+		element.classList.add("selected");
 	});
 };
